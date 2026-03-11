1a12ab2a2905082d7e4c629bd33cdc66c26dcc8b
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-11 15:28:38 +0900

    #1 feat : 온보딩 단계 객체 관리

--- src/views/onboarding/ui/OnboardingPage.tsx
+++ src/views/onboarding/ui/OnboardingPage.tsx
@@ -4,16 +4,30 @@ import { useRouter } from "next/navigation";
 import { Button } from "@/shared/ui/button";
 
 const TOTAL_STEPS = 3;
-
+const SLIDES = [
+  {
+    title: "함께 돌보는 반려생활",
+    description: "가족과 나누는 체크리스트로 실수 없이 제어하세요",
+  },
+  {
+    title: "시니어 맞춤케어",
+    description: `나이 들수록 더 세심하게 ${"\n"}상태에 맞춰 필요한 케어가 자동으로 준비돼요`,
+  },
+  {
+    title: "오늘 할 일을 한눈에\n지켜줄개로 시작해보세요",
+  },
+];
 export const OnboardingPage = () => {
   const router = useRouter();
   const { step, nextStep, prevStep } = useRegisterStep();
   return (
     <>
       <div className="flex flex-col w-full h-screen">
-        <div className="h-full flex flex-col items-center justify-center">
-          <div className="text-2xl font-bold">함께 돌보는 반려생활</div>
-          <p>가족과 나누는 체크리스트로 실수 없이 제어하세요</p>
+        <div className="h-full flex flex-col items-center justify-center whitespace-pre-line text-center">
+          <div className="text-2xl font-bold whitespace-pre-line">
+            {SLIDES[step].title}
+          </div>
+          {SLIDES[step].description && <p>{SLIDES[step].description}</p>}
           <div className="w-60 h-40 bg-gray-400 rounded-xl mt-4"></div>
           <div className="flex gap-2 m-2">
             {Array.from({ length: TOTAL_STEPS }).map((_, i) => (
