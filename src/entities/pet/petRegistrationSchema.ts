8e6b700962564736ad00cde890b952813a3b840f
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-13 02:28:35 +0900

    #4 feat : 유효성 검증 추가

--- src/entities/pet/petRegistrationSchema.ts
+++ src/entities/pet/petRegistrationSchema.ts
@@ -1,15 +1,24 @@
 import { z } from "zod";
 
 export const petRegistrationSchema = z.object({
-  name: z.string(),
+  name: z
+    .string()
+    .min(1, "이름을 입력해주세요")
+    .max(20, "이름은 20자 이하로 입력해주세요")
+    .regex(
+      /^[가-힣a-zA-Z0-9._\-]+$/,
+      "한글, 영문, 숫자, '.', '_', '-'만 사용 가능합니다",
+    ),
   birthDate: z.string(),
-  weight: z.number(),
+  weight: z
+    .number("숫자를 입력해주세요")
+    .positive("몸무게는 0보다 커야 합니다"),
   division: z.enum(["cat", "dog"]),
-  species: z.string(),
+  species: z.string().min(1).max(50).optional(),
   registrationNumber: z.number(),
   gender: z.enum(["male", "female"]),
   isNeutered: z.boolean(),
-  profileImageUrl: z.string(),
+  profileImageUrl: z.string().optional(),
   allergy: z.array(z.string()),
   diseases: z.array(z.string()),
 });
