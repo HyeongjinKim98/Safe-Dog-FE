0432077ba26ffe060efd677201510259c47e5068
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-13 02:16:59 +0900

    #3 feat : 로그인 샘플 페이지

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,16 +1,4 @@
-"use client";
-const oauth_url = `http://localhost:8080/oauth2/authorization`;
-const tmp_url = `http://localhost:3000/oauth/callback?accessToken=bearer&refreshToken=bearer`;
+import { LoginPage } from "@/views/login/ui/LoginPage";
 export default function page() {
-  const handleLogin = (provider: "google" | "naver" | "kakao") => {
-    window.location.href = tmp_url;
-    // window.location.href = `${oauth_url}/${provider}`;
-  };
-  return (
-    <div className="flex flex-col">
-      <button onClick={() => handleLogin("google")}> 구글 로그인</button>
-      <button onClick={() => handleLogin("naver")}> 네이버 로그인</button>
-      <button onClick={() => handleLogin("kakao")}> 카카오 로그인</button>
-    </div>
-  );
+  return <LoginPage />;
 }
---
+++ src/views/login/ui/LoginPage.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { Button } from "@/shared/ui/button";
+const oauth_url = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization`;
+export const LoginPage = () => {
+  const handleLogin = (provider: "GOOGLE" | "naver" | "kakao") => {
+    window.location.href = `${oauth_url}/GOOGLE`;
+  };
+  return (
+    <div className="flex flex-col w-full gap-2 p-4 items-center ">
+      <div className="w-40 h-40 bg-black rounded-full"></div>
+      <div className="whitespace-pre-line text-center font-semibold text-xl">
+        지켜줄개로 {"\n"} 아이의 건강을 지켜주세요
+      </div>
+      <Button
+        className="w-72 rounded-full h-12"
+        onClick={() => handleLogin("GOOGLE")}
+      >
+        구글 로그인
+      </Button>
+      <Button
+        className="w-72 rounded-full h-12"
+        onClick={() => handleLogin("naver")}
+      >
+        네이버 로그인
+      </Button>
+      <Button
+        className="w-72 rounded-full h-12"
+        onClick={() => handleLogin("kakao")}
+      >
+        카카오 로그인
+      </Button>
+    </div>
+  );
+};
