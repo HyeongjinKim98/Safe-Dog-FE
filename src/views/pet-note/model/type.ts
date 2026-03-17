6695a49fee4abb6efe89d73329cb8f1d2b483f5e
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-18 00:40:59 +0900

    #5 feat : 반려노트 type설정

---
+++ src/views/pet-note/model/type.ts
@@ -0,0 +1,71 @@
+export type TabStateType = "basic" | "disease" | "create";
+
+export type BasicCareType =
+  | "meal"
+  | "snack"
+  | "supplement"
+  | "water"
+  | "excretion"
+  | "walk"
+  | "weight"
+  | "grooming"
+  | "medicine"
+  | "vaccination"
+  | "etc";
+
+export type DiseaseCareType =
+  | "heart"
+  | "kidney"
+  | "cancer"
+  | "eye"
+  | "cushing"
+  | "arthritis"
+  | "other";
+
+export interface NoteState {
+  Tab: TabStateType;
+  selectedBasicItems: BasicCareType[];
+  selectedDiseaseItems: DiseaseCareType[];
+}
+
+export type TimeSlotType = "morning" | "lunch" | "dinner" | "custom";
+
+export type FoodType = "dry" | "wet";
+
+export type FoodUnitType = "g" | "cup" | "bowl" | "custom";
+
+export type SupplementUnitType = "count" | "pack" | "custom";
+
+export type WaterUnitType = "ml" | "cup" | "bowl" | "custom";
+
+export type WalkUnitType = "minute" | "hour" | "custom";
+
+export type MedicineUnitType = "tablet" | "pack" | "drop" | "bag" | "custom";
+
+export type VaccinationUnitType = "tablet" | "spoton" | "injection" | "custom";
+
+export type RepeatUnitType = "day" | "month" | "year";
+
+export type GroomingItemType =
+  | "grooming"
+  | "nail"
+  | "toothbrush"
+  | "earCleaning"
+  | "bath"
+  | "analGland"
+  | "custom";
+
+export type VaccinationType =
+  | "heartworm"
+  | "externalParasite"
+  | "deworming"
+  | "rabies"
+  | "custom";
+
+export type EtcType =
+  | "timeSlot"
+  | "amount"
+  | "image"
+  | "memo"
+  | "repeat"
+  | "simpleChecklist";
